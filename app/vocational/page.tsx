ecb804a8bd54eb456015edf5a8233e2c96872b3b
JakeTheHuman <zarins.jekabs@gmail.com>
2023-06-04 17:23:45 +0300

    feat: Added sort

--- app/vocational/page.tsx
+++ app/vocational/page.tsx
@@ -2,19 +2,39 @@
 
 import SchoolShort from '@/components/school-short'
 import Sort from '@/components/sort'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
-export default function Home() {
+export default async function Home() {
   const [sort, setSort] = useState<"OCE" | "populous" | "rank" | "distance">("OCE")
   const sortingCallback = (category: "OCE" | "populous" | "rank" | "distance") => {
     setSort(category)
   }
+  const schools = async () => {
+    const res = await fetch("http://scholastix.nav.lv:1280/", {
+      method: "POST",
+      body: JSON.stringify({
+        pagination: {size: 0, offset: 10},
+        targetlocation: {lat: 0, log: 0},
+        direction: "desc",
+        sortBy: "pupils"
+      }),
+    }).then(res => res.json()).catch(err => console.error(err))
+    console.log(res)
+  }
+
+  useEffect(() => {
+    schools()
+  }, [sort])
 
   return (
     <main>
-      <h1>Profesionālās</h1>
-      <Sort callback={sortingCallback} selected={sort} />
-      <SchoolShort distance={16} school='Rīgas Mākslas un Mediju tehnikums' population='1.0k' rating='20/302' score={123} id='123'/>
+      <div className="schools">
+        <h1>Profesionālās</h1>
+        <div className="search-section">
+          <Sort callback={sortingCallback} selected={sort} />
+        </div>
+        <SchoolShort distance={16} school='Rīgas Mākslas un Mediju tehnikums' population='1.0k' rating='20/302' score={123} id='123'/>
+      </div>
     </main>
   )
 }
--- components/sort.tsx
+++ components/sort.tsx
@@ -1,19 +1,24 @@
+'use client';
+
+import { useState } from "react";
+
 interface HeaderProps {
   callback: (category: "OCE" | "populous" | "rank" | "distance") => void,
   selected: "OCE" | "populous" | "rank" | "distance"
 }
 
 export default function Sort ({callback, selected}: HeaderProps) {
+  const [active, setActive] = useState<boolean>(false)
   return (
-    <div className="sort">
-      <div className="selected">
+    <div className={`sort ${active ? "active" : ""}`}>
+      <div className="selected" onClick={() => setActive(!active)}>
         <p>{selected}</p>
       </div>
       <div className="categories">
-        <button onClick={() => callback("OCE")}>OCE</button>
-        <button onClick={() => callback("populous")}>Populous</button>
-        <button onClick={() => callback("rank")}>Rank</button>
-        <button onClick={() => callback("distance")}>Distance</button>
+        {selected !== "OCE" && <button onClick={() => callback("OCE")}>OCE</button>}
+        {selected !== "populous" && <button onClick={() => callback("populous")}>Populous</button>}
+        {selected !== "rank" && <button onClick={() => callback("rank")}>Rank</button>}
+        {selected !== "distance" && <button onClick={() => callback("distance")}>Distance</button>}
       </div>
     </div>
   )
