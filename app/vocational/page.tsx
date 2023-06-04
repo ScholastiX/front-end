3f6e6cd9e820a2853c021f4617c11e52b452c7e7
JakeTheHuman <zarins.jekabs@gmail.com>
2023-06-04 20:53:29 +0300

    feat: Added filter

--- app/vocational/page.tsx
+++ app/vocational/page.tsx
@@ -1,11 +1,58 @@
 'use client'
 
+import Filter from '@/components/filter'
 import SchoolShort from '@/components/school-short'
 import Sort from '@/components/sort'
 import { useEffect, useState } from 'react'
 
 export default async function Home() {
   const [sort, setSort] = useState<"OCE" | "populous" | "rank" | "distance">("OCE")
+  let filter = {
+    pupils: {
+      min: 0,
+      max: 1000
+    },
+    distance: {
+      min: 0,
+      max: 1000
+    },
+    rating: {
+      min: 0,
+      max: 1000
+    },
+    profession: ""
+  }
+
+  const filterCallback = (set: "pupils" | "distance" | "rating", minMax: "min" | "max", value: number) => {
+    if (minMax === "min" && value > filter[set].max) return;
+    if (minMax === "max" && value < filter[set].min) return;
+    if (value < 0) return;
+    if (minMax === "min") {
+      filter = {
+        ...filter,
+        [set]: {
+          min: value,
+          max: filter[set].max
+        }
+      }
+    } else {
+      filter = {
+        ...filter,
+        [set]: {
+          min: filter[set].min,
+          max: value
+        }
+      }
+    }
+  }
+
+  const filterProfessionCallback = (profession: string) => {
+    filter = {
+      ...filter,
+      profession
+    }
+  }
+
   const sortingCallback = (category: "OCE" | "populous" | "rank" | "distance") => {
     setSort(category)
   }
@@ -14,7 +61,7 @@ export default async function Home() {
       method: "POST",
       body: JSON.stringify({
         pagination: {size: 0, offset: 10},
-        targetlocation: {lat: 0, log: 0},
+        targetLocation: {lat: 0, log: 0},
         direction: "desc",
         sortBy: "pupils"
       }),
@@ -22,9 +69,9 @@ export default async function Home() {
     console.log(res)
   }
 
-  useEffect(() => {
-    schools()
-  }, [sort])
+  // useEffect(() => {
+  //   schools()
+  // }, [sort])
 
   return (
     <main>
@@ -32,6 +79,7 @@ export default async function Home() {
         <h1>Profesionālās</h1>
         <div className="search-section">
           <Sort callback={sortingCallback} selected={sort} />
+          <Filter callback={filterCallback} callbackProfession={filterProfessionCallback} defaultFilter={filter} />
         </div>
         <SchoolShort distance={16} school='Rīgas Mākslas un Mediju tehnikums' population='1.0k' rating='20/302' score={123} id='123'/>
       </div>
---
+++ components/filter.tsx
@@ -0,0 +1,68 @@
+'use client';
+
+import { useState } from "react";
+
+interface HeaderProps {
+  callback: (set: "pupils" | "distance" | "rating", minMax: "min" | "max", value: number) => void,
+  callbackProfession: (profession: string) => void,
+  defaultFilter: {
+    pupils: {
+      min: number,
+      max: number
+    },
+    distance: {
+      min: number,
+      max: number
+    },
+    rating: {
+      min: number,
+      max: number
+    },
+    profession: string
+  }
+}
+
+export default function Filter ({callback, callbackProfession, defaultFilter}: HeaderProps) {
+  const [active, setActive] = useState<boolean>(false)
+
+  return <div className={`filter ${active ? "active" : ""}`}>
+    <p onClick={() => setActive(!active)} className="title">Filtri</p>
+    <div className="pupils">
+      <p>Skolēnu skaits</p>
+      <div className="min">
+        <label htmlFor="min-pupils">Min</label>
+        <input type="number" id="min-pupils" defaultValue={defaultFilter.pupils.min} onChange={(e) => {if (!isNaN(parseInt(e.target.value))) callback("pupils", "min", parseInt(e.target.value))}} />
+      </div>
+      <div className="max">
+        <label htmlFor="max-pupils">Max</label>
+        <input type="number" id="max-pupils" defaultValue={defaultFilter.pupils.max} onChange={(e) => {if (!isNaN(parseInt(e.target.value))) callback("pupils", "max", parseInt(e.target.value))}} />
+      </div>
+    </div>
+    <div className="distance">
+      <p>Attālums</p>
+      <div className="min">
+        <label htmlFor="min-distance">Min</label>
+        <input type="number" id="min-distance" defaultValue={defaultFilter.distance.min} onChange={(e) => {if (!isNaN(parseInt(e.target.value))) callback("distance", "min", parseInt(e.target.value))}} />
+      </div>
+      <div className="max">
+        <label htmlFor="max-distance">Max</label>
+        <input type="number" id="max-distance" defaultValue={defaultFilter.distance.max} onChange={(e) => {if (!isNaN(parseInt(e.target.value))) callback("distance", "max", parseInt(e.target.value))}} />
+      </div>
+    </div>
+    <div className="rating">
+      <p>Reitings</p>
+      <div className="min">
+        <label htmlFor="min-rating">Min</label>
+        <input type="number" id="min-rating" defaultValue={defaultFilter.rating.min} onChange={(e) => {if (!isNaN(parseInt(e.target.value))) callback("rating", "min", parseInt(e.target.value))}} />
+      </div>
+      <div className="max">
+        <label htmlFor="max-rating">Max</label>
+        <input type="number" id="max-rating" defaultValue={defaultFilter.rating.max} onChange={(e) => {if (!isNaN(parseInt(e.target.value))) callback("rating", "max", parseInt(e.target.value))}} />
+      </div>
+    </div>
+    <div className="profession">
+      <p>Profesija</p>
+      <input type="text" defaultValue={""} onChange={(e) => callbackProfession(e.target.value)} />
+    </div>
+  </div>
+}
