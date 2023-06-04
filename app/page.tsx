04ff9eea21eae731f60522abc7965c3959a94ea9
CodedSakura <sakuracoded@gmail.com>
2023-06-04 22:11:06 +0300

    Add about section to main page

--- app/page.tsx
+++ app/page.tsx
@@ -19,6 +19,23 @@ export default function Home() {
           <Button text='Vecāks' type='secondary' arrow='left' onClick={() => {}} size='large' />
         </div>
       </div>
+      <div className="about">
+        <div className="spacer"></div>
+        <h2>Par mums:</h2>
+        <p>
+          Sveicināti skolu lokatora Latvijā vietnē.
+        </p>
+        <p>
+          Mēs atbalstām apzinātu lēmumu pieņemšanu attiecībā uz iegūstamo izglītību, un mūsu vietne nodrošina iespēju iegūt lietotājam viegli pārskatāmu un salīdzināmu informāciju par mācību iestādēm Latvijā.
+        </p>
+        <p>
+          Mūsu mērķis ir palīdzēt atrast to mācību iestādi, kas vislabāk atbilst tam karjeras virzienam, kādā vēlies dodies!
+        </p>
+        <p>
+          Atrodi un aplūko skolas, kas nodrošina izgītības iegūšanu no 6. līdz 12. klasei, lai ar pārliecību orientētos izglītības piedāvājumā.
+        </p>
+        <div className="spacer"></div>
+      </div>
     </main>
   )
 }
